b9fae0298055f480d1692309fd08e5d314cef4d4
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-11-25 14:32:35 +0200

    custom control placement

--- apps/base/src/app/app.element.ts
+++ apps/base/src/app/app.element.ts
@@ -14,12 +14,21 @@ export class AppElement extends HTMLElement {
     loader
       .load()
       .then((google) => {
-        const mapOptions = {
+        const mapOptions: google.maps.MapOptions = {
           center: {
             lat: 0,
             lng: 0,
           },
           zoom: 4,
+          scrollwheel: true,
+          panControl: false,
+          mapTypeControl: true,
+          zoomControl: true,
+          streetViewControl: false,
+          scaleControl: true,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM,
+          },
           mapTypeControlOptions: {
             mapTypeIds: [
               'roadmap',
@@ -28,8 +37,15 @@ export class AppElement extends HTMLElement {
               'terrain',
               'styled_map',
             ],
+            position: google.maps.ControlPosition.TOP_LEFT,
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+          },
+          fullscreenControl: true,
+          fullscreenControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_BOTTOM,
           },
         };
+
         const map = new google.maps.Map(
           document.getElementById('map'),
           mapOptions
@@ -41,13 +57,50 @@ export class AppElement extends HTMLElement {
 
         map.mapTypes.set('styled_map', styledMapType);
         map.setMapTypeId('styled_map');
+
+        const btn = document.getElementById('control-one') as HTMLButtonElement;
+        const btn2 = document.getElementById(
+          'control-two'
+        ) as HTMLButtonElement;
+
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(btn);
+        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(btn2);
+
+        let markerEnabled = false;
+
+        btn.addEventListener('click', () => {
+          map.setZoom(8);
+          map.setCenter({ lat: 37.7749, lng: 23 });
+        });
+
+        btn2.addEventListener('click', () => {
+          markerEnabled = !markerEnabled;
+        });
+
+        google.maps.event.addListener(map, 'click', (event) => {
+          if (markerEnabled) {
+            new google.maps.Marker({
+              position: event.latLng,
+              map,
+              title: 'Hi there!',
+            });
+          }
+        });
       })
       .catch((e) => {
         // do something
       });
+
     this.innerHTML = `
       <div id="map"></div>
+      <button class="control" id="control-one"> 
+        Show Greece
+      </button>
+      <button class="control" id="control-two">
+        Add marker
+      </button>
       `;
   }
 }
+
 customElements.define('my-map-root', AppElement);
